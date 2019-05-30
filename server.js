6abfe2d08c589b809cc1d6c4fa2d2cf179a2ce7e
Glitch (stellar-noise) <none>
2019-05-30 09:56:13 +0000

    🍢⛪️ Checkpoint ./server.js:3938417/570

--- server.js
+++ server.js
@@ -6,12 +6,14 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 
+
+app.use(bodyParser.json());
+
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
 app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
 
-app.use(bodyParser.json());
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
@@ -30,13 +32,13 @@ app.get("/api/hello", function (req, res) {
 
 // WHOamI API endpoint
 app.get('/api/whoami',(req, res, next)=>{
-  var browser_lang = req.header['Accept-Language'];
+  
+  var browser_lang = req.headers['accept-language'];
   var user_spec = req.header['User-Agent'];
-  var ip = req.ip
+  var ip = req.ip;
   
-  var jsonObj = {"ipaddress":ip,"language": browser_lang, "software": user_spec};
   
-  res.json(jsonObj);
+  res.json({"ipaddress": req.ip,"language": browser_lang,"software": user_spec});
 });
 
 
